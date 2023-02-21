bea26cc218a7b0564def13fe0039be341655e2b5
jwvonk <jwavonk@gmail.com>
2023-02-20 20:24:09 -0800

    Made Push & Raise Distances Exact
    Probably a waste of time but I did it anyway

--- Assets/Scripts/BoatPushScript.cs
+++ Assets/Scripts/BoatPushScript.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class BoatPushScript : MonoBehaviour
 {
 
     public Transform _playerTransform;
+    public Transform _fluidTransform;
     public float pushSpeed = 10;
     public float pushDist = 10;
-    private bool fallen = false;
+    public float riseSpeed = 50;
+    public float riseDist = 10;
     void Start()
     {
         _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _fluidTransform = GameObject.FindGameObjectWithTag("Fluid").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -24,20 +29,61 @@ public class BoatPushScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
+        StartCoroutine(raise());
         StartCoroutine(push());
+
     }
 
     private IEnumerator push()
     {
-        Debug.Log("Function Called");
-        Vector3 impact = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Vector3 direction = _playerTransform.position - transform.position;
+        Vector3 impact = transform.position;
+        impact.y = 0;
+
+
+        Vector3 direction = _playerTransform.position - impact;
+        direction.y = 0;
+
         direction.Normalize();
-        while (Vector3.Distance(_playerTransform.position, impact) < pushDist)
+
+        Vector3 dest = impact + (direction * pushDist);
+
+
+        while (Vector3.Distance(new Vector3(_playerTransform.position.x, 0, _playerTransform.position.z), dest) > 0)
+
         {
-            _playerTransform.position += (direction * pushSpeed) * Time.deltaTime;
+            Vector3 postPush = _playerTransform.position + direction * pushSpeed * Time.deltaTime;
+            postPush.y = 0;
+            if (Vector3.Distance(postPush, impact) > pushDist)
+            {
+                _playerTransform.position = new Vector3(dest.x, _playerTransform.position.y, dest.z);
+            }
+            else
+            {
+                _playerTransform.position = new Vector3(postPush.x, _playerTransform.position.y, postPush.z);
+     
+            }
             yield return null;
         }
+        Destroy(gameObject);
+    }
 
+    private IEnumerator raise()
+    {
+        float targetLevel = _fluidTransform.position.y + riseDist;
+        while (_fluidTransform.position.y < targetLevel)
+        {
+            if (_fluidTransform.position.y + (riseSpeed * Time.deltaTime) < targetLevel)
+            {
+                _playerTransform.position += new Vector3(0f, riseSpeed, 0f) * Time.deltaTime;
+                _fluidTransform.position += new Vector3(0f, riseSpeed, 0f) * Time.deltaTime;
+            }
+            else
+            {
+                _playerTransform.position = new Vector3(_playerTransform.position.x, targetLevel, _playerTransform.position.z);
+                _fluidTransform.position = new Vector3(_fluidTransform.position.x, targetLevel, _fluidTransform.position.z);
+            }
+               
+            yield return null;
+        }
     }
 }
